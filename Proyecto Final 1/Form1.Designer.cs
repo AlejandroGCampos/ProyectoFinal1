8f1aae6b68312e10576bdeb32621babab25b989b
Alejandro Godoy Campos <egodoyc4@miumg.edu.gt>
2025-05-21 19:01:26 -0600

    Add files via upload

---
+++ Proyecto Final 1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Proyecto_Final_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.btnInvestigar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(135, 161);
+            this.txtBuscar.Multiline = true;
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(347, 34);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(135, 281);
+            this.txtRespuesta.Multiline = true;
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(347, 231);
+            this.txtRespuesta.TabIndex = 1;
+            // 
+            // btnInvestigar
+            // 
+            this.btnInvestigar.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnInvestigar.Location = new System.Drawing.Point(152, 225);
+            this.btnInvestigar.Name = "btnInvestigar";
+            this.btnInvestigar.Size = new System.Drawing.Size(314, 30);
+            this.btnInvestigar.TabIndex = 2;
+            this.btnInvestigar.Text = "Investigar y Generar Documentos";
+            this.btnInvestigar.UseVisualStyleBackColor = true;
+            this.btnInvestigar.Click += new System.EventHandler(this.btnInvestigar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(618, 570);
+            this.Controls.Add(this.btnInvestigar);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.txtBuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.Button btnInvestigar;
+    }
+}
+
